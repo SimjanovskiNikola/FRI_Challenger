3dda75fe061167f6c8cc428e4f4ee6f5702f03c7
Nikola Simjanovski <nikolcesimjanovski@gmail.com>
2025-01-31 16:53:24 +0000

    DCE-33: Generate Keys While Adding and Removing Peaces

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,4 @@
-use engine::{
-    attacks::pext::{gen_bishop_mov, gen_moves, BISHOP_MASKS},
-    game::Game,
-    shared::helper_func::{const_utility::SqPos, print_utility::print_bitboard},
-};
-use std::{arch::x86_64::_pext_u64, thread::Builder};
+use engine::game::Game;
 
 pub mod engine;
 
