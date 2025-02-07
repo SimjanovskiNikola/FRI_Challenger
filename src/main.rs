6f061da4a0091eb31bd6aeec41609b197404449d
Nikola Simjanovski <nikolcesimjanovski@gmail.com>
2025-02-07 18:08:05 +0000

    DCE-36: Update Internal_Move

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,7 @@
-use engine::{
-    game::Game, move_generation::perft::init_test_func,
-    shared::helper_func::const_utility::FEN_START,
-};
+use engine::game::Game;
 
 pub mod engine;
 
 fn main() {
     let _game = Game::initialize();
-    let stats = init_test_func(&FEN_START, 5, true);
-    assert_eq!(stats.nodes, 4865609);
 }
