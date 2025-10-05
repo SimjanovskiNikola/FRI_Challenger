312e56fccd4da6c27235076c641b01193297d084
Nikola Simjanovski <nikolcesimjanovski@gmail.com>
2025-10-05 20:07:12 +0000

    DCE-119: Better Time Managment

--- src/engine/protocols/time.rs
+++ src/engine/protocols/time.rs
@@ -20,11 +20,9 @@ pub fn safe_to_start_next_iter(search: &Search) -> bool {
 
     let elapsed = search.uci.start_time.elapsed();
     let total_time = search.uci.time_limit.unwrap_or(Duration::from_millis(u64::MAX));
-    let remaining_time = total_time - elapsed;
-    let threshold = remaining_time.mul_f32(0.4); // 40% of remaining time
 
     // Allow another iteration only if less than 40% of remaining time has been used
-    elapsed < (total_time - threshold)
+    elapsed.mul_f32(2.5) < total_time
 }
 
 #[inline(always)]
